acc1fbd76d0bccd2d5a98f02bcc45ad4481031bc
Lluis Boria <lluisb.gomez@gmail.com>
2021-03-27 02:25:44 +0100

    no message

--- boria-music/src/pages/bio/bio.js
+++ boria-music/src/pages/bio/bio.js
@@ -1,18 +1,23 @@
-import { NavBar} from '../../components/navbar/navBar'
-import lluis from '../../components/assets/lluis1.jpeg'
+import { BsEnvelopeOpen } from 'react-icons/bs'
 import './bio.css'
+
 export const Bio = () => {
 
     return (
         <div className='grid-bio'>
-            <div className='navbar'>
-                <NavBar />
-            </div>
+            <div className='navbar2 '>
+                <button className='vertical-button '>TOURDATES</button>
+                  </div> 
+
+            <div className='navbar3' >
+                <button className='vertical-button'>SHOP</button>
+                  </div> 
+               
             <div className='bio-text'>
-                <p className='p3'>A singer/songwriter and guitarist born in Barcelona, BORIA makes a blending dark indie-folk with synthesizer and pedal effects right through his acoustic guitar painting melancholy portraits of life, death, and love.</p>
+                <p className='p3'>"A singer/songwriter and guitarist born in Barcelona, BORIA makes a blending dark indie-folk with synthesizer and pedal effects right through his acoustic guitar painting melancholy portraits of life, death, and love."</p>
             </div>
-            <div className='image'>
-                <img src={lluis} className='filtered' alt='lluis'></img>
+            <div className='contact-icon'>
+                <BsEnvelopeOpen />
             </div>
         </div>
     )
--- boria-music/src/pages/home/home.js
+++ boria-music/src/pages/home/home.js
@@ -3,6 +3,7 @@ import './home.css'
 import { Footer } from '../../components/footer/footer'
 import { NavBar } from '../../components/navbar/navBar'
 import {IoIosArrowDown } from 'react-icons/io'
+
 export const Home = () => {
     
     const fadeInSubtitle = useSpring({config: { duration: 4000}, from: { opacity: 1 }, to: { opacity: 0 }})
@@ -13,21 +14,23 @@ export const Home = () => {
         <div className='grid-home'>
             <animated.div  style={fadeInLinks} className='navbar p2'>
                 <NavBar   />
-            </animated.div>
+                    </animated.div>
+                    
             <div className='boria'>
                 <animated.h2 style={fadeInHome} >BORIA</animated.h2>
-            </div>
+                    </div>
 
             <div className=' boria subtitle'>
                 <animated.p className='p2' style={fadeInSubtitle}>WELCOME! TO THE OFFICAL WEBSITE.</animated.p>
-            </div>
+                    </div>
 
             <animated.div  style={fadeInLinks} className='social-links'>
                 <Footer  ></Footer>
-            </animated.div>
+                    </animated.div>
+
             <div className='go-to-album'>
                 <IoIosArrowDown className='arrow-down'></IoIosArrowDown>
-            </div>
+                    </div>
         </div>
     )
 }
